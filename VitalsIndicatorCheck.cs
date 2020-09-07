57c4dd15bf3a0e03332426da23a1e0dfd003cf4a
AdarshSrivatsa98 <adarsh.98srivatsa@gmail.com>
2020-09-08 00:17:11 +0530

    adding tests to check vitals

--- VitalsIndicatorCheck.cs
+++ VitalsIndicatorCheck.cs
@@ -6,8 +6,13 @@ using static VitalsLibraries.VitalsIndicator;
 class VitalsIndicatorCheck
 {
     static int Main() {
-        ExpectTrue(vitalsAreOk(100, 95, 60));
         ExpectFalse(vitalsAreOk(40, 91, 92));
+        ExpectFalse(vitalsAreOk(175, 100, 60));
+        ExpectFalse(vitalsAreOk(100, 85, 92));
+        ExpectFalse(vitalsAreOk(100, 91, 25));
+        ExpectFalse(vitalsAreOk(100, 100, 100));
+        ExpectTrue(vitalsAreOk(100, 95, 60));
+
         Console.WriteLine("All ok");
         return 0;
     }
