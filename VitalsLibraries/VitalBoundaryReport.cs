a2c1cf7bc21bb13d850433ec46959e87aba9e340
AdarshSrivatsa98 <adarsh.98srivatsa@gmail.com>
2020-09-08 14:23:32 +0530

    cyclometric complexity-II

--- VitalsLibraries/VitalBoundaryReport.cs
+++ VitalsLibraries/VitalBoundaryReport.cs
@@ -10,17 +10,14 @@ namespace VitalsLibraries
         public static bool WhenBpmGivenThenReturnFalseForCriticalVital(float bpm) {
             if (bpm < 70 || bpm > 150)
                 return false;
-            else return true;
         }
         public static bool WhenSpo2GivenThenReturnFalseForCriticalVital(float spo2) {
             if (spo2 < 90)
                 return false;
-            else return true;
         }
         public static bool WhenRespRateGivenThenReturnFalseForCriticalVital(float respRate) {
             if (respRate < 30 || respRate > 95)
                 return false;
-            else return true;
         }
     }
 }
