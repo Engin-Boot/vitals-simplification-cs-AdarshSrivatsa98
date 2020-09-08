1dfd3acdbebfca0c0777a2d67b9f08edd319ccb7
AdarshSrivatsa98 <adarsh.98srivatsa@gmail.com>
2020-09-08 14:19:25 +0530

    cyclometric complexity

---
+++ VitalsLibraries/VitalBoundaryReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VitalsLibraries
+{
+    class VitalBoundaryReport
+    {
+        public static bool WhenBpmGivenThenReturnFalseForCriticalVital(float bpm) {
+            if (bpm < 70 || bpm > 150)
+                return false;
+            else return true;
+        }
+        public static bool WhenSpo2GivenThenReturnFalseForCriticalVital(float spo2) {
+            if (spo2 < 90)
+                return false;
+            else return true;
+        }
+        public static bool WhenRespRateGivenThenReturnFalseForCriticalVital(float respRate) {
+            if (respRate < 30 || respRate > 95)
+                return false;
+            else return true;
+        }
+    }
+}
--- VitalsLibraries/VitalsIndicator.cs
+++ VitalsLibraries/VitalsIndicator.cs
@@ -2,26 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using static VitalsLibraries.VitalBoundaryReport;
 namespace VitalsLibraries
 {
     class VitalsIndicator
-    {
+    {   
+        
         public static bool vitalsAreOk(float bpm, float spo2, float respRate)
-        {
-            if (bpm < 70 || bpm > 150)
-            {
-                return false;
-            }
-            else if (spo2 < 90)
-            {
-                return false;
-            }
-            else if (respRate < 30 || respRate > 95)
-            {
-                return false;
-            }
-            return true;
+        {   
+            bool vitalFlag = 
+            WhenBpmGivenThenReturnFalseForCriticalVital(bpm) &&
+            WhenSpo2GivenThenReturnFalseForCriticalVital(spo2) &&
+            WhenRespRateGivenThenReturnFalseForCriticalVital(respRate);
+
+            return vitalFlag;
         }
     }
 }
